01f7cd5d56195181fa23d49568d1027393a74e8c
Matt Muenster <matt.muenster@gmail.com>
2015-03-15 18:47:28 -0500

    Minor mods

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT }} 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
 
   var host = server.address().address
   var port = server.address().port
