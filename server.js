1129f42faa7b34a83fc0a7a1f428fa180ff0434a
Matt Muenster <matt.muenster@gmail.com>
2015-03-16 07:54:40 -0500

    Updated responses

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
 	console.log(request.body)
-	response.send("Yo mamma dude!", req.body)
+	response.send("Yo mamma dude!", request.body)
 });
 
-app.put('/', function (req, res) {
-  console.log(req.body.A);
+app.put('/', function (request, response) {
+  console.log(request.body.A);
 
-  res.send('Got a put request', req.body);
+  response.send('Got a put request', request.body);
 })
 
 app.listen(app.get('port'), function() {
